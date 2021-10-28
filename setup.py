33775f391527683d33bac2abdc79b718f63554dc
peerakarn <peerakarn.j@siametrics.com>
2021-10-28 23:34:12 +0700

    cycle gan

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='img_task_toolbox',
-    version='1.0',
+    version='1.0.0',
     description='random code from other great people that being use frequently in my projects',
     author='KKQanT',
-    packages=install_requires,
+    author_email="asskarnwin@gmail.com",
+    packages=['img_task_toolbox'],
+    install_requires=install_requires,
 )
