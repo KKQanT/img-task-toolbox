5fbbd37c0edb4b2f92fb8307134848718f21ce54
peerakarn <peerakarn.j@siametrics.com>
2021-10-28 23:13:02 +0700

    cycle gan

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt', 'r') as f:
     install_requires = f.read().splitlines()
 
 setup(
-    name='img_clf_toolbox',
+    name='img_task_toolbox',
     version='1.0',
     description='random code from other great people that being use frequently in my projects',
     author='KKQanT',
